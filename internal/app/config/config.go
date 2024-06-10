f8f294638540405b0e27836f613ba376d70ef4ea
a.cherepanov <a.cherepanov@robo.finance>
2024-06-10 14:54:40 +0700

    iter4

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -10,8 +10,8 @@ type Options struct {
 func ParseArgs() *Options {
 	options := new(Options)
 
-	flag.StringVar(&options.ServerUrl, "a", "localhost:8000", "адрес запуска HTTP-сервера")
-	flag.StringVar(&options.ShortUrl, "b", "http://localhost:8000/", "базовый адрес результирующего шортлинка")
+	flag.StringVar(&options.ServerUrl, "a", "localhost:8080", "адрес запуска HTTP-сервера")
+	flag.StringVar(&options.ShortUrl, "b", "http://localhost:8080/", "базовый адрес результирующего шортлинка")
 	flag.Parse()
 
 	return options
